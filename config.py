e2e87a1e208fd8ba16cd00727a333a89a441bf0e
Jeison Rosario <jeisonrosario5@gmail.com>
2024-01-01 19:17:46 -0300

    Update

---
+++ config.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+from decouple import config as GETENV
+
+
+class DevelopmentConfig:
+    """
+    object | DevelopmentConfig: Contains configurations required for the project
+    """
+
+    DEBUG = True
+    MYSQL_HOST = GETENV("HOST")
+    MYSQL_USER = GETENV("USER")
+    MYSQL_PASSWORD = password = GETENV("PASSWORD")
+    MYSQL_DB = GETENV("DATABASE")
+
+
+config = {"development": DevelopmentConfig}
