d8bca1e01aabb208fdc10dba246958d30537e022
jeisonrosario66 <jeisonrosario5@gmail.com>
2024-02-05 01:14:32 -0300

    Added "@app.route" route administrator

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ from src.models.artist_controller import ArtistController
 app = Flask(__name__)
 artist_controller = ArtistController()
 
+
 @app.errorhandler(404)
 def handle_not_found(e):
     return jsonify({"Error 404": f"{msg_exception(handle_not_found,e)}"}), 404
@@ -22,33 +23,34 @@ def handle_not_found405(e):
     return jsonify({"Error 405": f"{msg_exception(handle_internal_server_error,e)}"}), 405
 
 # ---------------------------------------------------------------------------------------------
-
-@app.route("/", methods=["GET"])
+@app.route("/", methods=["GET", "POST"])
 def list_artists_show():
-    print(request.method)
+    response_got = artist_controller.list_artists()
     if request.method == "GET":
-        response_got = artist_controller.list_artists()
         return render_template("index.html",response = response_got)
-
-
-@app.route("/artist_ajax", methods=["POST"])
-def name():
     if request.method == "POST":
-        response_got = artist_controller.get_artist()
+        return render_template("table.html",response = response_got)
+    
+    
+@app.route("/artist", methods=["POST"])
+def artist_show():
+
+    artist_name = request.form["search"]
+    if request.method == "POST":
+        response_got = artist_controller.get_artist(artist_name)
         if not response_got == None:
             return render_template("table.html",response2 = response_got)
         else:
             return render_template("artist_not_found.html")
 
+@app.route("/artist_update", methods=["PUT"])
+def artist_update():
+    if request.method == "PUT":
+        if request.is_json:
+            artist_data = request.get_json()
+            artist_key = artist_data["artist_key"]
+            response_got = artist_controller.update_artist(artist_key, artist_data)
+            return  "response_got"
+
 if __name__ == "__main__":
-
-    # Route configuration using the controller
-    # app.route("/artists",methods=["GET","POST"])(artist_controller.list_artists)
-    app.route("/api/artist", methods=["POST"])(artist_controller.get_artist)
-
-    # app.route("/artist", methods=["POST"])(artist_controller.add_artist)
-    
-    # app.route("/artist/<code>", methods=["PUT"])(artist_controller.update_artist)
-    # app.route("/artist/<code>", methods=["DELETE"])(artist_controller.delete_artist)
-
     app.run()
