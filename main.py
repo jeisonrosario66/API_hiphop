13a4ecddd67398b8634935824597be891fb87e97
jeisonrosario66 <jeisonrosario5@gmail.com>
2024-04-17 21:44:11 -0400

    Add login system
    A login-admin system is added, now some functions will be available only with login

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
 # -*- coding: utf-8 -*-
-from flask import Flask, request
+import os
+from flask import Flask, request, session
 from flask import jsonify
 from flask import Flask, render_template
 from src.errors_handling.msg_exception import msg_exception
 from src.models.artist_controller import ArtistController
+from src.models.login_controller import LoginController
 
 # Flask app instantiation
 app = Flask(__name__)
+# for flask session
+app.secret_key = os.urandom(24)
+
 artist_controller = ArtistController()
+login_controller = LoginController()
 
 @app.errorhandler(404)
 def handle_not_found(e):
@@ -40,6 +46,7 @@ def list_artists_show():
     # "requested_with" will get the header from the request
     requested_with = request.headers.get('X-Requested-With')
     response_got = artist_controller.list_artists()
+
     if request.method == "GET" :
         if requested_with == "popstate_event": # Value of header
             return render_template("table.html",response = response_got)
@@ -76,14 +83,15 @@ def artist_show(code):
             return render_template("table.html",response2 = response_got)
         else:
             if not response_got == None:
-                return render_template("artist_table.html",response3 = response_got)
+                return render_template("artist_table.html",response2 = response_got)
             else:
                 return render_template("artist_not_found.html", artist_name = artist_name)
 
 @app.route("/artist", methods=["POST"])
 def artist_show2():
     if request.method == "POST":
-        artist_name = request.form["search"]
+        artist_name = request.get_json()
+        artist_name = artist_name["search"]
         response_got = artist_controller.get_artist(artist_name)
         if not response_got == None:
             return render_template("table.html",response2 = response_got)
@@ -108,29 +116,68 @@ def artist_update():
 def add_artist():
     """Add a new artist
     """
-    requested_with = request.headers.get('X-Requested-With')
-  
-    if request.method == "GET":
-        if requested_with == "popstate_event":
-            artist_name = str(request.args.keys())
-            artist_name = artist_name.replace("dict_keys(['{", "")
-            artist_name = artist_name.replace('"artistData":"', "")
-            artist_name = artist_name.replace("'])", "")
-            artist_name = artist_name.replace('"}', "")
-            response_got = artist_controller.get_artist(artist_name)
-            if response_got != None:
-                return f"'{artist_name}' already exists"
+    if session.get('user'):
+        requested_with = request.headers.get('X-Requested-With')
+    
+        if request.method == "GET":
+            if requested_with == "popstate_event":
+                artist_name = str(request.args.keys())
+                artist_name = artist_name.replace("dict_keys(['{", "")
+                artist_name = artist_name.replace('"artistData":"', "")
+                artist_name = artist_name.replace("'])", "")
+                artist_name = artist_name.replace('"}', "")
+                response_got = artist_controller.get_artist(artist_name)
+                if response_got != None:
+                    return f"'{artist_name}' already exists"
+                else:
+                    return f"'{artist_name}' available"
+                
             else:
-                return f"'{artist_name}' available"
+                return render_template("add_artist.html")
             
-        else:
-            return render_template("add_artist.html")
-        
 
-    elif request.method == "POST":
-        new_artist_data = request.json["artistData"]
-        response_got = artist_controller.add_artist(new_artist_data)
-        return response_got
+        elif request.method == "POST":
+            new_artist_data = request.json["artistData"]
+            response_got = artist_controller.add_artist(new_artist_data)
+            return response_got
+    else:
+        return render_template("session_not_active.html")
+    
+@app.route("/login", methods=["GET"])
+def login():
+    return render_template("login.html")
+
+@app.route("/login_verification", methods=["POST"])
+def login_verification():
+    """This function will verify if the username and password exist in the database
+
+    Returns:
+        template: Session activated or not
+    """
+    login_data = request.get_json()
+    login_data = login_data["search"]
+
+    login_data_user = login_data["user"]
+    login_data_pass = login_data["pass"]
+
+    #access_login return false or true
+    access_login = login_controller.login_verify(login_data_user,login_data_pass)
+
+
+    response_got = artist_controller.list_artists()
+    print(access_login)
+    if access_login == 1:
+        session["user"] = login_data_user
+        return render_template("index.html",response = response_got)
+    else:
+        return ""
+    
+@app.route("/logout", methods=["GET"])
+def logout():
+    response_got = artist_controller.list_artists()
+    session.pop('user', None)
+    return render_template("index.html",response = response_got)
+
     
 if __name__ == "__main__":
     # Allows external connection in development
